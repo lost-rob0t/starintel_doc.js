6ad5b32d209d9031b40675316c1ad2b73bf9c252
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-25 17:43:24 -0400

    Expose the v0.9 schema bundle and dtype field registry

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const document = require("./document");
 const schemaOrg = require("./schema-org");
+const schemaBundle = require("./schema-bundle");
 const validation = require("./validation");
 
 module.exports = {
   ...document,
   ...schemaOrg,
   ...validation,
+  baseSchema: schemaBundle.baseSchema,
+  expansion: schemaBundle.expansion,
+  manifest: schemaBundle.manifest,
+  verifySchemaBundle: schemaBundle.verifyBundle,
+  materializeSchema: schemaBundle.materializeSchema,
+  fieldsForDtype: schemaBundle.fieldNamesForDtype,
   get schema() {
     return validation.loadSchema();
+  },
+  get schemaRevision() {
+    return schemaBundle.manifest.schema_revision;
+  },
+  get schemaHash() {
+    return schemaBundle.manifest.expansion_content_hash;
+  },
+  get profile() {
+    return schemaBundle.manifest.profile;
   },
   get dtypes() {
     return validation.loadSchema().properties.dtype.enum.slice();
